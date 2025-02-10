74311367202a98db73e19a1829177f1e44fe452a
Susovan <susovan@gmail.com>
2025-02-10 21:35:53 +0530

    test push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import './App.css';
+const isMobile = () => {
+  return /Mobi|Android|iPhone/i.test(navigator.userAgent);
+};
 
 function App() {
+  useEffect(() => {
+    if (isMobile()) {
+      window.location.href = "https://m.cricbuzz.com/";
+    }
+  }, []);
   return (
     <>
-    <h1 className='text-3xl text-purple-600 text-center'>The site is coming soon</h1>
+      <h1 className='text-3xl text-purple-600 text-center'>The site is coming soon</h1>
     </>
   );
 }
